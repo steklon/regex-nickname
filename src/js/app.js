3a2c29799d152d566b6b74a877a50732365bdcc3
steklon <steklo_one@mail.ru>
2024-12-05 13:17:25 +1000

    removed comments in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,5 @@
-// import { Bowman } from "./Character";
-// import { characters } from "./characters";
-
-// const user = new Bowman("user");
-
-// console.log(user);
-// console.log(characters);
-
 import Validator from "./usernameValidator";
 
 const user = new Validator();
-
 console.log(user.validateUsername("User_123-name"));
 
